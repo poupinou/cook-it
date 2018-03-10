d7483deaa81e0c33e91a80a8ea4e47e7eaefe272
S3boune <seb.morizot@gmail.com>
2018-03-10 14:37:03 +0100

    seb - add SHOW Recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -25,6 +25,10 @@ class RecipesController < ApplicationController
 		redirect_to recipes_path
 	end
 
+	def show
+		@recipe = Recipe.find(params[:id])
+	end
+
 	def destroy
 		Recipe.destroy(params[:id])
 		redirect_to recipes_path
