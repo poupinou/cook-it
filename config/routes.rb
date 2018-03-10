19a217a113a038ada8cc4d2d449bd427d64c67eb
S3boune <seb.morizot@gmail.com>
2018-03-10 09:43:36 +0100

    seb - add init ADMIN

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-	
+
   devise_for :users
   root 'static_pages#home'
+
+	get '/admin', to: 'static_pages#admin', as: 'admin'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
