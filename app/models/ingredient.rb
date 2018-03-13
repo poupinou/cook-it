8c8dc0f29d30037176c57ed07700236b505fe989
S3boune <seb.morizot@gmail.com>
2018-03-13 09:50:31 +0100

    seb - modif LIAISON DB

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,6 @@
 class Ingredient < ApplicationRecord
   has_many :ingredient_to_recipe
-  has_many :recipe, through: :ingredient_to_recipe
+  has_many :recipes, through: :ingredient_to_recipe
 
   validates :name, presence: true, uniqueness: true
 
