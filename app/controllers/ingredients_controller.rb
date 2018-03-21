41020c82d86d072a80fd39480479bbf92c47551e
Nicolas Roulleau <nicolas.roulleau@gmail.com>
2018-03-21 14:49:39 +0100

    securite ingredient ok

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -16,6 +16,13 @@ class IngredientsController < ApplicationController
 					@value = params[:nom]
 				end
 			end
+
+		else
+      		if user_signed_in?
+        		redirect_to root_path
+      		else
+        		redirect_to new_user_session_path
+      		end
 		end
 
 		##############################################
